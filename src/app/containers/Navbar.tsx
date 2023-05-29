8c2290853a909ee8f3c9aa3847ce2cabee64c5d6
Artemides <edmpulasky@gmail.com>
2023-05-29 16:07:33 -0500

    connect account

--- src/app/containers/Navbar.tsx
+++ src/app/containers/Navbar.tsx
@@ -1,17 +1,10 @@
-import { Button } from "@/components/Button";
 import React from "react";
-import { useMoralis } from "react-moralis";
+import NavbarTools from "./NavbarTools";
 
 export const Navbar = () => {
-  const { enableWeb3, account } = useMoralis();
-
-  const handleWalletConnect = async () => {
-    await enableWeb3();
-  };
   return (
     <nav className=" w-screen bg-slate-900 flex px-16 py-4 justify-center">
-      {" "}
-      <Button handleClick={handleWalletConnect} text="Connect" />
+      <NavbarTools />
     </nav>
   );
 };
---
+++ src/app/containers/NavbarTools.tsx
@@ -0,0 +1,55 @@
+import { Button } from "@/components/Button";
+import React, { useEffect } from "react";
+import { useMoralis } from "react-moralis";
+
+const NavbarTools = () => {
+  const { enableWeb3, account, isWeb3Enabled, Moralis, deactivateWeb3 } =
+    useMoralis();
+
+  const handleWalletConnect = async () => {
+    try {
+      await enableWeb3();
+      localStorage.setItem("connection", "injected");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    Moralis.onAccountChanged((account) => {
+      if (account) return;
+
+      localStorage.removeItem("connection");
+      deactivateWeb3();
+    });
+  }, [Moralis, deactivateWeb3]);
+
+  const diyplayRecortedAccount = (account: string) => {
+    return (
+      <span className="text-bold text-green-400">
+        {`${account.slice(0, 6)}...${account.slice(-4)}`}
+      </span>
+    );
+  };
+
+  useEffect(() => {
+    const autoConnect = async () => {
+      if (localStorage.getItem("connect")) return;
+      console.log("enabling account");
+      await enableWeb3();
+    };
+    autoConnect();
+  }, [isWeb3Enabled, enableWeb3]);
+
+  return (
+    <div className="flex align-middle">
+      {account ? (
+        diyplayRecortedAccount(account)
+      ) : (
+        <Button handleClick={handleWalletConnect} text="Connect" />
+      )}
+    </div>
+  );
+};
+
+export default NavbarTools;
